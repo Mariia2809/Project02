f13c189a298e6f5da3ab2e930d8e855b1cee70b1
gr421_pomdm <popovamariia2809@gmail.com>
2023-11-13 12:47:51 +0700

    все

--- locallibrary/locallibrary/catalog/urls.py
+++ locallibrary/locallibrary/catalog/urls.py
@@ -1,13 +1,13 @@
-#from import about as about
+
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import include
+
 from django.urls import path
 from . import views
-from django.contrib import admin
 from .views import validate_username
 from django.contrib.auth.views import LoginView
-
+from django.contrib.auth.views import LogoutView
+from django.urls import re_path as url
 from .views import *
 
 urlpatterns = [
@@ -23,13 +23,12 @@ urlpatterns = [
     path('creating/', views.MyPostListViews.as_view(), name='creating'),
     path('deleting/', views.ApplicationDelete.as_view(), name='deleting'),
 
-
     path('admin_base/', views.ApplicationListViewAdmin.as_view(), name='admin_base'),
     path('category/', views.CategoryView.as_view(), name='category'),
     path('category/<int:pk>/delete/', views.CategoryDelete.as_view(), name='category_delete'),
-    path('category/<int:pk>/creating/', views.CategoryCreate.as_view(), name='creating_category'),
+    path('creating_category/', views.CategoryCreate.as_view(), name='creating_category'),
     path('change/<int:pk>/status/', views.ChangeStatusRequest.as_view(), name='change_status'),
+    path('request/<int:pk>/delete/', views.ApplicationDelete.as_view(), name='application_confirm_delete')
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
 
